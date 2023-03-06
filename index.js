9f2dcf6477355f68f56f631807938140f4270b45
salih <muhammetsalihaslan@gmail.com>
2023-03-06 12:02:49 +0300

    graph q

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+// import { ApolloServer } from "@apollo/server";
+// import { startStandaloneServer } from "@apollo/server/standalone";
 import { users, posts, comments } from "./data.js";
 import { nanoid } from "nanoid";
 
